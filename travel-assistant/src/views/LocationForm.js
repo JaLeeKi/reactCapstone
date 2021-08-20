0ede1a0e41ccb3b664ca88294283eeaf54e6778d
JaLeeKi <jadekirkham@gmail.com>
2021-08-19 20:27:32 -0600

    Pushing for the day, fixing form on home page

--- travel-assistant/src/views/LocationForm.js
+++ travel-assistant/src/views/LocationForm.js
@@ -1,44 +1,93 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const onSubmit = async () => {
-  const options = {
-    method: "GET",
-    url: "https://priceline-com.p.rapidapi.com/hotels/city/search",
-    params: { q: "Seattle" },
-    headers: {
-      "x-rapidapi-key": "a249296ae8msh4ba546f035043f1p1e7ae7jsn460dcaf9557d",
-      "x-rapidapi-host": "priceline-com.p.rapidapi.com",
-    },
+export default function LocationForm() {
+  const [searchParams, setSearchParams] = useState({
+    guests: 1,
+    city: "",
+    startDate: "2021-08-19",
+    endDate: "2021-08-20",
+  });
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { value, className } = e.target;
+
+    setSearchParams({
+      ...searchParams,
+      [className]: value,
+    });
   };
 
-  await axios
-    .request(options)
-    .then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-};
+  // useEffect(() => {
+  //   const options = {
+  //     method: "GET",
+  //     url: "https://priceline-com-provider.p.rapidapi.com/v1/hotels/locations",
+  //     params: { name: "New York" },
+  //     headers: {
+  //       "x-rapidapi-host": "priceline-com-provider.p.rapidapi.com",
+  //       "x-rapidapi-key": "a249296ae8msh4ba546f035043f1p1e7ae7jsn460dcaf9557d",
+  //     },
+  //   };
 
-export default function LocationForm() {
+  //   axios
+  //     .request(options)
+  //     .then(function (response) {
+  //       console.log(response.data);
+  //     })
+  //     .catch(function (error) {
+  //       console.error(error);
+  //     });
+  // });
+
   return (
     <div>
-      <form action="/hotel" onSubmit={onSubmit} method="get">
-        <input type="text" placeholder="Where Would You Like To Go?"></input>
+      <form
+        action="/hotel"
+        onSubmit={() => setSearchParams(searchParams.city)}
+        method="get"
+      >
+        <input
+          value={searchParams.city}
+          className="city"
+          name="city"
+          onChange={handleChange}
+          type="text"
+          placeholder="Where Would You Like To Go?"
+        ></input>
         <div>
           <label for="guests">How Many Guests?</label>
           <br />
-          <input type="number" class="guests"></input>
+          <input
+            type="number"
+            onChange={handleChange}
+            value={searchParams.guests}
+            className="guests"
+            name="guests"
+          ></input>
         </div>
         <div>
           <label for="date">When Would You Like To Travel?</label>
           <br />
-          <input type="date" class="date"></input>
-          <input type="date"></input>
+          <input
+            type="date"
+            onChange={handleChange}
+            value={searchParams.startDate}
+            className="startDate"
+            name="startDate"
+          ></input>
+          <input
+            type="date"
+            className="endDate"
+            name="endDate"
+            onChange={handleChange}
+            value={searchParams.endDate}
+          ></input>
         </div>
         <input type="submit" value="Submit" />
+      </form>
+      <form>
+        <h1>{searchParams === undefined ? searchParams : null}</h1>
       </form>
     </div>
   );
